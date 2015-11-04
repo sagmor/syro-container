2b5e9cc730d1073e63eb6096ae115246631fa225
Seba Gamboa <me@sagmor.com>
2015-11-04 14:41:08 -0300

    Improved docs

--- lib/syro/container.rb
+++ lib/syro/container.rb
@@ -4,11 +4,25 @@ require 'dry/container'
 require "syro/container/version"
 
 class Syro
+
+  # Container module for `Syro::Deck`
+  #
+  # @example
+  #     class MyApp < Syro::Deck
+  #       include Syro::Container
+  #     end
+  #
+  #     # then:
+  #     MyApp.register('path', Syro.new {})
+  #
   module Container
     def self.included(deck)
       deck.extend ClassMethods
     end
 
+    # Overrides default `Syro::Deck` initialization to always fetch sub apps
+    # from container
+    #
     def initialize(before)
       code = Proc.new do
         instance_eval(&before) if before
@@ -18,6 +32,23 @@ class Syro
       super(code)
     end
 
+    # Manually resolve from container.
+    #
+    # Used when you want to add routes after the resolution
+    #
+    # @example
+    #     class MyApp < Syro::Deck
+    #       include Syro::Container
+    #     end
+    #
+    #     Syro.new(MyApp) {
+    #       resolve!
+    #
+    #       on('after') {
+    #         # Whatever
+    #       }
+    #     }
+    #
     def resolve!
       return if @resolved
       @resolved = true
@@ -29,14 +60,20 @@ class Syro
     rescue Dry::Container::Error
     end
 
+    # Class methods extended when {Syro::Container} is included
     module ClassMethods
 
+      # Container accessor
       def container
         @container ||= Dry::Container.new
       end
 
-      def register(*args)
-        container.register(*args)
+      # Register an app into the container.
+      #
+      # @param path [String] Path component to register.
+      # @param app [Syro] Syro app to register at the path.
+      def register(path,app)
+        container.register(path,app)
       end
 
     end
